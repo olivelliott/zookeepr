6c6e190dfafdb55700df27d0231b8eca2d3fc7da
Olive <olivelliott48@gmail.com>
2022-06-20 22:06:38 -0400

    Create function to handle specific requests for one animal

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ function filterByQuery(query, animalsArray) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -46,6 +51,15 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 })
 
 
@@ -53,5 +67,5 @@ app.get('/api/animals', (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log(`]['API server now on port${PORT}`);
+    console.log(`API server now on port${PORT}`);
 });
