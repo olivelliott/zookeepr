50b580e6e4626d2ac8e34483949bf4787257dcf9
Olive <olivelliott48@gmail.com>
2022-06-22 21:27:16 -0400

    Create routes to serve front end pages, finish GET requests to serve page data

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
+
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -110,9 +113,21 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
 
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port${PORT}`);
